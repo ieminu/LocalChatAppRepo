0aef5087450817510233087ece8128a76c42a35e
ieminu <yamenhasanaga3@gmail.com>
2023-09-12 20:39:25 +0300

    UI & Some Changes
    - UI and some small things changed

---
+++ LocalChatApp/LocalChatApp/Migrations/20230912163349_PropertyNameChanged.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LocalChatApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class PropertyNameChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MessageProp",
+                table: "Messages",
+                newName: "UsernameAndMessage");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UsernameAndMessage",
+                table: "Messages",
+                newName: "MessageProp");
+        }
+    }
+}
---
+++ LocalChatApp/LocalChatApp/wwwroot/js/chat.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (username, message) {
+    var dt = document.createElement("dt");
+    var dd = document.createElement("dd");
+
+    document.getElementById("messagesList").appendChild(dt);
+    document.getElementById("messagesList").appendChild(dd);
+
+    dt.textContent = username + ":";
+    dd.textContent = message;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    let username = document.getElementById("usernameInput").value;
+    let message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", username, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+
+    $.ajax({
+        type: "GET",
+        data: { "username": username, "message": message }
+    });
+});
